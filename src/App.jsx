6bb7ecc783164059b119c322a16f920f5dc2555d
Aman Pandey <amanpandey7728@gmail.com>
2024-12-22 17:29:22 +0530

    Add code:

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import EventMenu from "./pages/EventMenu"
 const App=()=>{
     const [pageData,setPageData]=useState("");
     return (
-        <div className="max-w-screen overflow-x-hidden min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <div className="max-w-screen overflow-x-hidden min-h-screen flex flex-col font-inter">
         <Header className="w-full" page={pageData}/>
         <Router>
             <Routes>
