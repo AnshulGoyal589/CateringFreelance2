961d0451608b37c1c9a53358277665f0d5a1a9d2
Nipun Yadav <nipunyadav@Nipuns-MacBook-Air.local>
2024-12-25 13:35:22 +0530

    Fixed the footer i think

--- src/components/Header/Preview/index.jsx
+++ src/components/Header/Preview/index.jsx
@@ -4,7 +4,8 @@ const Preview=(props)=>{
     return (
     props.page!='n/a'?
             <div style={{backgroundImage:`url(./${props.page.replace(' ','')}.png)`}} className="preview">
-                <p>Home &nbsp; {"/"} &nbsp; Services &nbsp;{props.page!="Services"?`   / ${props.page}`:null}</p>
+                <p>Home {props.page!='Contact Us' && props.page!='About Us'?"/ Services":null}
+                    &nbsp;{props.page!="Services"?`/ ${props.page}`:null}</p>
                 <text>{props.page}</text>
             </div>
     :
--- src/pages/EventMenu/index.jsx
+++ src/pages/EventMenu/index.jsx
@@ -1,10 +1,8 @@
 import { useEffect,useRef} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css"; 
-import MenuCard from "./MenuCard";
 import Course from "./Course"
 import "./eventmenu.css";
-
 const EventMenu = (props) => {
     const scrollRef=useRef(null);
     console.log(props.eventType)
@@ -18,6 +16,15 @@ const EventMenu = (props) => {
     return (
     <div className="menu-container"
     >
+        <div 
+        style={{
+            marginBottom:"-5%", overflow:"hidden",
+            display:"flex", alignItems:"center",justifyContent:"center",height:"250px",width:"100%",backgroundImage:"url('MenuTextBorder.png')", backgroundSize:"100%"}}>
+           <p className="gold-lustrous-text">
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             officia deserunt.
+           </p>
+        </div>
         <Course  name="Starters" color="yelloworange"/>
         <Course  name="Appetizers" color="#f78d4a"/>
         <Course  name="Main Course" color="maroon"/>
